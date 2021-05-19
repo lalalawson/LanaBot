50f8d7e3fc4eeb677a9c82c0998bb9cfb677909a
lalalawson <lawson.tkw@gmail.com>
2021-05-19 17:52:08 +0800

    added replykeyboard markup

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import telegram
+from telegram import KeyboardButton
 from telegram.ext import Updater, CommandHandler
 import os
 from dotenv import load_dotenv
+from telegram.replykeyboardmarkup import ReplyKeyboardMarkup
 
 load_dotenv()
 
@@ -9,7 +10,12 @@ allowed_users = ["lalalawson", "linawoo"]
 
 def start(update, context):
     username = update.effective_user.username
-    update.message.reply_text("Hello " + username)
+    button_row1 = [KeyboardButton("Show me handsome guys!"), KeyboardButton("Show me something cute!")]
+    button_row2 = [KeyboardButton("Tell me a joke!"), KeyboardButton("I just wna rant...")]
+    buttons = [button_row1, button_row2]
+    reply_keyboard = ReplyKeyboardMarkup(buttons, one_time_keyboard=True)
+    update.message.reply_text("Hello " + username + "!\n" + \
+        "This bot was created to commerate our first year together 🥳\n What do you require today? 😚", reply_markup=reply_keyboard)
 
 
 def main():
