d52d08de1bf072b236bc4a2df4eb0057cb49d486
lalalawson <lawson.tkw@gmail.com>
2021-05-19 18:54:10 +0800

    checked validity of reply & validity of users

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from telegram import KeyboardButton
-from telegram.ext import Updater, CommandHandler
+from telegram import KeyboardButton, user
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, MessageFilter
 import os
 from dotenv import load_dotenv
 from telegram.replykeyboardmarkup import ReplyKeyboardMarkup
@@ -8,15 +8,30 @@ load_dotenv()
 
 allowed_users = ["lalalawson", "linawoo"]
 
-def start(update, context):
-    username = update.effective_user.username
-    button_row1 = [KeyboardButton("Show me handsome guys!"), KeyboardButton("Show me something cute!")]
-    button_row2 = [KeyboardButton("Tell me a joke!"), KeyboardButton("I just wna rant...")]
-    buttons = [button_row1, button_row2]
-    reply_keyboard = ReplyKeyboardMarkup(buttons, one_time_keyboard=True)
-    update.message.reply_text("Hello " + username + "!\n" + \
-        "This bot was created to commerate our first year together 🥳\n What do you require today? 😚", reply_markup=reply_keyboard)
+message_options = ["Show me handsome guys!", "Show me something cute!", "Tell me a joke!", "I just wna rant..."]
 
+def start(update, context):
+    global reply_keyboard 
+    username = update.effective_user.username
+    button_row1 = [KeyboardButton(message_options[0]), KeyboardButton(message_options[1])]
+    button_row2 = [KeyboardButton(message_options[2]), KeyboardButton(message_options[3])]
+    buttons = [button_row1, button_row2]
+
+    # perform check on allowed users
+    if username in allowed_users:
+        reply_keyboard = ReplyKeyboardMarkup(buttons, one_time_keyboard=True)
+        update.message.reply_text("Hello " + username + "!\n" + \
+            "This bot was created to commerate our first year together 🥳\n" + \
+            "What do you require today? 😚", reply_markup=reply_keyboard)
+    else:
+        update.message.reply_text("Sorry " + username + "! This is a private bot so it's not available for your viewing! 😅")
+
+def illegal_user(update, context):
+    username = update.effective_user.username
+    update.message.reply_text("Sorry " + username + "! This is a private bot so it's not available for your viewing! 😅")
+
+def illegal_option(update, context):
+    update.message.reply_text("I'm sorry dear! There's no such option.. 😅 Select a proper one from the reply keyboard!", reply_markup=reply_keyboard)
 
 def main():
     TOKEN = os.getenv("API_TOKEN")
@@ -24,8 +39,12 @@ def main():
     
     dispatcher = updater.dispatcher
 
-    # commands
+    # commands handler
     dispatcher.add_handler(CommandHandler("start", start))
+
+    # message handler
+    dispatcher.add_handler(MessageHandler(~Filters.chat(username=allowed_users), illegal_user))
+    dispatcher.add_handler(MessageHandler(~(Filters.regex(message_options[0]) ^ Filters.regex(message_options[1]) ^ Filters.regex(message_options[2]) ^ Filters.regex(message_options[3])), illegal_option))
 
     updater.start_polling()
     updater.idle()
